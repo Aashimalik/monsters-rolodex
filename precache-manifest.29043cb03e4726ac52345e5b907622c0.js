726ef7e3a6678f1278f169dc6487255c920e9517
ayesha.malik <ayesha.malik@allocatesoftware.com>
2020-01-06 11:54:04 +0530

    Updates

---
+++ precache-manifest.29043cb03e4726ac52345e5b907622c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a538b8a4b4247fdafdeb3baff74524f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f62cff56d9af31398209",
+    "url": "/monsters-rolodex/static/css/main.dd5b120b.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f62cff56d9af31398209",
+    "url": "/monsters-rolodex/static/js/main.d528feab.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
